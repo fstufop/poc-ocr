3a037134209582f481cea893735bebd785f45c3c
FilipeTeodoro <fstufop@gmail.com>
2026-07-28 22:07:43 -0300

    feat: mount SwaggerModule at /docs for non-production envs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -24,6 +25,19 @@ async function bootstrap() {
 
   app.enableCors();
   app.enableShutdownHooks();
+
+  if (configService.get('env') !== 'production') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('POC OCR API')
+      .setVersion('1.0')
+      .addApiKey(
+        { type: 'apiKey', in: 'header', name: 'x-api-key' },
+        'x-api-key',
+      )
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('docs', app, document);
+  }
 
   await app.listen(port);
   Logger.log(
